9be4c9e2b595050018e13495f58ee33596274aa7
SATYAPU NITEESH KUMAR <82573249+33niteesh@users.noreply.github.com>
2023-09-12 20:50:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,66 @@ function App(props) {
      setPopup(false)
   }
   const getDatafrom=()=>{
-    axios.get("http://localhost:3001/data").then(e=>setData(e.data));
+    var table = [
+         {
+        "id":1,
+        "first_name":"Arjun",
+        "SIZE":"S",
+        "last_name":"Kapoor"
+    },
+    {
+        "id":2,
+        "first_name":"Rajesh",
+        "SIZE":"S",
+        "last_name":"Kumar"
+    },
+    {
+        "id":3,
+        "first_name":"Nithin",
+        "SIZE":"M",
+        "last_name":"Ghatkari"
+    },{
+        "id":4,
+        "first_name":"Ram",
+        "SIZE":"M",
+        "last_name":"Kaushik"
+    },{
+        "id":5,
+        "first_name":"Bipin",
+        "SIZE":"L",
+        "last_name":"Patel"
+    },{
+        "id":6,
+        "first_name":"Raju",
+        "SIZE":"M",
+        "last_name":"Singh"
+    },
+    {
+        "id":7,
+        "first_name":"Vickey",
+        "SIZE":"S",
+        "last_name":"Mudiraj"
+    },
+    {
+        "id":8,
+        "first_name":"Sai Kumar",
+        "SIZE":"L",
+        "last_name":"Naidu"
+    },
+    {
+        "id":9,
+        "first_name":"Madhu",
+        "SIZE":"M",
+        "last_name":"Roy"
+    },{
+        "id":10,
+        "first_name":"Jaishwal",
+        "SIZE":"L",
+        "last_name":"Harith"
+    }
+    ]
+    setData(table);
+  };
   }
   useEffect(()=>{
     getDatafrom();
